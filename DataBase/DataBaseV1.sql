43d50937fdf21051ce7d85b735de194eae178ea9
Luis Fernando Camargo Pineda <fenilol293@gmail.com>
2019-04-05 14:42:51 -0500

    Se actualiza query de base de datos

--- DataBase/DataBaseV1.sql
+++ DataBase/DataBaseV1.sql
@@ -1,70 +1,70 @@
-CREATE DATABASE Hola 
-GO
+CREATE DATABASE Prueba
 
-USE Test;
+USE Prueba;
 
-CREATE TABLE Hola.compañia (
-    id_compañia int PRIMARY KEY,
+CREATE TABLE compañia (
+    id_compañia int IDENTITY(1,1) PRIMARY KEY,
     nombre_compañia varchar(20) NOT NULL,
     descripcion varchar(200),
 	estado bit,
 );
 
-CREATE TABLE Hola.rol_seg(
-	id_rol int PRIMARY KEY,
+CREATE TABLE rol_seg(
+	id_rol int IDENTITY(1,1) PRIMARY KEY,
 	nombre_rol varchar(50) NOT NULL,
 	descripcion varchar(200),
 	estado bit,
 );
 
-CREATE TABLE Hola.producto(
-	id_producto int PRIMARY KEY,
+CREATE TABLE producto(
+	id_producto int IDENTITY(1,1) PRIMARY KEY,
 	nombre_producto varchar(50) NOT NULL,
 	descripcion varchar(200),
 	estado bit,
-	nombre_compañia varchar(20) NOT NULL,
-	nombre_rol varchar(50) NOT NULL,
-	FOREIGN KEY (nombre_compañia) REFERENCES Prueba.compañia (nombre_compañia),
-	FOREIGN KEY (nombre_rol) REFERENCES Prueba.rol_seg (nombre_rol),
+	id_compañia int NOT NULL,
+	id_rol int NOT NULL,
+	FOREIGN KEY (id_compañia) REFERENCES compañia (id_compañia),
+	FOREIGN KEY (id_rol) REFERENCES rol_seg (id_rol),
 );
 
-CREATE TABLE Hola.tipo_document(
-	id_tipo_document int PRIMARY KEY,
+CREATE TABLE tipo_document(
+	id_tipo_document int IDENTITY(1,1) PRIMARY KEY,
 	nombre_document varchar(20) NOT NULL,
 	estado bit,
 );
 
-CREATE TABLE Hola.departamento(
-	id_departamento int PRIMARY KEY,
+CREATE TABLE departamento(
+	id_departamento int IDENTITY(1,1) PRIMARY KEY,
 	nombre_departamento varchar(20) NOT NULL,
 	estado bit,
 );
 
-CREATE TABLE Hola.ciudad(
-	id_ciudad int PRIMARY KEY,
+CREATE TABLE ciudad(
+	id_ciudad int IDENTITY(1,1) PRIMARY KEY,
 	nombre_ciudad varchar(20) NOT NULL,
 	estado bit,
 );
 
-CREATE TABLE Hola.venta(
-	id_venta int PRIMARY KEY,
-	nombre_producto varchar(50) NOT NULL,
+CREATE TABLE venta(
+	id_venta int IDENTITY(1,1),
+	id_producto int NOT NULL,
 	primer_nombre varchar(20) NOT NULL,
 	segundo_nombre varchar(20),
 	primer_apellido varchar(20) NOT NULL,
 	segundo_apellido  varchar(20),
-	tipo_document varchar(20) NOT NULL,
+	id_tipo_document int NOT NULL,
 	numero_document int,
 	correo_electronico varchar(100) NOT NULL,
 	telefono int NOT NULL,
-	nombre_departamento varchar(20) NOT NULL,
-	nombre_ciudad varchar(20) NOT NULL,
+	id_departamento int NOT NULL,
+	id_ciudad int NOT NULL,
 	observacion varchar(500),
-	FOREIGN KEY (nombre_producto) REFERENCES Prueba.producto (nombre_producto),
-	FOREIGN KEY (tipo_document) REFERENCES Prueba.tipo_document (nombre_document),
-	FOREIGN KEY (nombre_departamento) REFERENCES Prueba.departamento (nombre_departamento),
-	FOREIGN KEY (nombre_ciudad) REFERENCES Prueba.ciudad (nombre_ciudad),
+	FOREIGN KEY (id_producto) REFERENCES producto (id_producto),
+	FOREIGN KEY (id_tipo_document) REFERENCES tipo_document (id_tipo_document),
+	FOREIGN KEY (id_departamento) REFERENCES departamento (id_departamento),
+	FOREIGN KEY (id_ciudad) REFERENCES ciudad (id_ciudad),
+	PRIMARY KEY (id_venta, id_producto, id_tipo_document, id_departamento, id_ciudad),
 );
-GO
 
-drop table Hola;
+
+drop table Prueba;
